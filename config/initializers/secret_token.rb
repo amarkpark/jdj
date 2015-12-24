6bb571a841b2c364536a00bc0403a26bf70a4373
amarkpark <nospam@amarkpark.com>
2015-12-24 23:50:46 +0000

    Initial commit. ~AMP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jdj::Application.config.secret_key_base = '0c24c278935e2a6af0d053bf66fd4c68ea5d4ffc2ffd937bda97a54461f217f4e7307d622fd9e678ae87b96ec0859f3ae747e93d33f8d412cc8a57ea63fe5144'
